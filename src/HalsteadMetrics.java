1fdcccca758a0b2a5918fa95b47812b6e72a826f
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-05-07 22:50:16 +0200

    Added keywords and operations

--- src/HalsteadMetrics.java
+++ src/HalsteadMetrics.java
@@ -9,11 +9,17 @@ import java.util.HashMap;
 public class HalsteadMetrics {
 
 	/** 
-	 * List of Operators (Java operators and types)
+	 * List of Operators (Java keywords and operators)
 	 */
 	String[] operatorsList = {"if","else","for","while","{","}","case","Class",
-			"catch","try","final","static","public","private","protected",
-			"=","+","-","?",":","/","\\","*",".","break","do"};
+			"catch","try","finally","final","throws","throw","static","public","private","protected",
+			"=","+","-","?","!","%",">","<",":","/","\\","*",".","&","^","|","~",
+			"break","do","continue",
+			"instanceof","long","native","new","this","package","interface",
+			"return","short","super","strictfp","switch","synchronized",
+			"transient","volatile","void","goto","extends","implements",
+			"byte","assert","boolean","char","const","double","int","float",
+			"enum"};
 	
 	HashMap<String,Integer> operatorsHash;
 	
