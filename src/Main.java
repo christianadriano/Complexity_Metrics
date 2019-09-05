916d0a70ed143c5f91659646af98fc020c47deff
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-09-05 18:31:20 +0200

    Update Main.java
    Loads file and counts operators and operands

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import java.util.HashMap;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Computes Halstead metrics of 
@@ -8,6 +11,9 @@ import java.util.HashMap;
  * @author Chris Adriano
  *
  */
+
+import java.util.*;
+
 public class Main {
 
 	/** 
@@ -24,7 +30,10 @@ public class Main {
 			"byte","assert","boolean","char","const","double","int","float","short",
 			"enum"};
 	
-	String[] operatorsList = {"=","+","-","?","!","%",">","<",":","/","\\","*",".","&","^","|","~"};
+	String[] operatorsList = {"=","+","-","?","!","%",">","<","*","&","^","|","~"};
+	
+	String[] stopKeysList = {",",";",":","/","//","."};
+
 	
 	HashMap<String,Integer> operatorsHash;
 	
@@ -43,6 +52,46 @@ public class Main {
 		}
 	}
 	
+	public List<String> readFileToList(String filePath) {
+		try {
+			return(Files.readAllLines(Paths.get(filePath)));
+		}
+		catch(Exception e) {
+			return null;
+		}
+	}
+
+	
+	private void load(String filePath) {
+		List<String> list_lines = this.readFileToList(filePath);
+		for(String line:list_lines) {
+			//tokenize it
+			String[] token_list = line.split("");
+			for(String token:token_list) {
+				if(this.operatorsHash.containsKey(token)) {
+					Integer value_obj = this.operatorsHash.get(token);
+					value_obj = new Integer(value_obj.intValue() + 1); 
+					this.operatorsHash.put(token, value_obj);
+				}
+				else {
+					if(this.operandsHash.containsKey(token)) {
+						Integer value_obj = this.operandsHash.get(token);
+						value_obj = new Integer(value_obj.intValue() + 1); 
+						this.operandsHash.put(token, value_obj);
+					}	
+					else {
+						this.operandsHash.put(token, new Integer(1));
+					}
+				}	
+			}
+		}
+	}
+	
+	public static void main(String args[]) {
+		Main main = new Main();
+		main.load("C:\\Users\\Christian\\Documents\\GitHub\\Complexity_Metrics\\test\\translate.java")
+	}
+
 	
 	
 }
