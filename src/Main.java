5642f23335bae5bb19b9379804450e4a28da2079
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-09-06 15:34:52 +0200

    Update Main.java
    Isolated double operators

--- src/Main.java
+++ src/Main.java
@@ -24,35 +24,44 @@ public class Main {
 			"catch","try","finally","final","throws","throw","return",
 			"static","public","private","protected",
 			"break","do","continue","goto",
-			"instanceof","long","native","new","this","package","interface",
+			"instanceof","long","native","new","null","this","package","interface",
 			"super","strictfp","synchronized",
 			"transient","volatile","void","extends","implements",
 			"byte","assert","boolean","char","const","double","int","float","short",
-			"enum"};
-	
-	String[] operatorsList = {"=","+","-","?","!","%",">","<","*","&","^","|","~"};
-	
+	"enum"};
+
+	String[] singleOperatorsList = {"=","+","-","?","!","%",">","<","*","&","^","|","~"};
+
+	String[] doubleOperatorsList = {"==","++","+=","--","-=","!=","&&","||"};
+
 	String[] stopKeysList = {",",";",":","/","//",".","(",")","[","]","{","}"};
 
-	
-	HashMap<String,Integer> operatorsHash;
-	
+
+	HashMap<String,Integer> doubleOperatorsHash;
+	HashMap<String,Integer> singleOperatorsHash;
+	HashMap<String,Integer> stopKeyHash;
 	HashMap<String,Integer> operandsHash;
-		
+
 	/**  
 	 * Constructor initializes the data structure with operators
 	 */
 	public Main() {
-		this.operatorsHash = new HashMap<String,Integer>();
-		for(String operator:operatorsList) {
-			this.operatorsHash.put(operator, new Integer(0));
+		this.singleOperatorsHash = new HashMap<String,Integer>();
+		this.doubleOperatorsHash = new HashMap<String,Integer>();
+		this.stopKeyHash = new HashMap<String,Integer>();
+
+		for(String operator:singleOperatorsList) {
+			this.singleOperatorsHash.put(operator, new Integer(0));
+		}
+		for(String operator:doubleOperatorsList) {
+			this.doubleOperatorsHash.put(operator, new Integer(0));
 		}
 		for(String keyword:keywordsList) {
-			this.operatorsHash.put(keyword, new Integer(0));
+			this.singleOperatorsHash.put(keyword, new Integer(0));
 		}
 		this.operandsHash = new HashMap<String,Integer>();
 	}
-	
+
 	public List<String> readFileToList(String filePath) {
 		try {
 			return(Files.readAllLines(Paths.get(filePath)));
@@ -62,52 +71,68 @@ public class Main {
 		}
 	}
 
-	
+
+
+	public String isolateDoubleOperators(String line) {
+		for(String doubleOperator: this.doubleOperatorsList) {
+			line = line.replace(doubleOperator, " "+doubleOperator+" ");
+		}
+		return line;
+	}
+
 	public String substituteStopWords(String line) {
 		for(String stopWord: this.stopKeysList) {
 			line = line.replace(stopWord, " ");
 		}
 		return line;
 	}
-	
+
 	private void load(String filePath) {
 		List<String> list_lines = this.readFileToList(filePath);
 		for(String line:list_lines) {
 			//tokenize it
 			line = this.substituteStopWords(line);
+			line = this.isolateDoubleOperators(line);
+			
 			String[] token_list = line.split(" ");
 			for(String token:token_list) {
-				if(this.operatorsHash.containsKey(token)) {
-					Integer value_obj = this.operatorsHash.get(token);
+				if(this.singleOperatorsHash.containsKey(token)) {
+					Integer value_obj = this.singleOperatorsHash.get(token);
 					value_obj = new Integer(value_obj.intValue() + 1); 
-					this.operatorsHash.put(token, value_obj);
+					this.singleOperatorsHash.put(token, value_obj);
 				}
-				else {
-					if(this.operandsHash.containsKey(token)) {
-						Integer value_obj = this.operandsHash.get(token);
+				else
+					if(this.doubleOperatorsHash.containsKey(token)) {
+						Integer value_obj = this.doubleOperatorsHash.get(token);
 						value_obj = new Integer(value_obj.intValue() + 1); 
-						this.operandsHash.put(token, value_obj);
-					}	
-					else {
-						this.operandsHash.put(token, new Integer(1));
+						this.doubleOperatorsHash.put(token, value_obj);
 					}
-				}	
+					else {
+						if(this.operandsHash.containsKey(token)) {
+							Integer value_obj = this.operandsHash.get(token);
+							value_obj = new Integer(value_obj.intValue() + 1); 
+							this.operandsHash.put(token, value_obj);
+						}	
+						else {
+							this.operandsHash.put(token, new Integer(1));
+						}
+					}	
 			}
 		}
 	}
-	
+
 	private void print() {
-		
 		System.out.println(this.operandsHash.toString());
-		System.out.println(this.operatorsHash.toString());
+		System.out.println(this.singleOperatorsHash.toString());
+		System.out.println(this.doubleOperatorsHash.toString());
 
 	}
-	
+
 	public static void main(String args[]) {
 		Main main = new Main();
 		main.load("C:\\Users\\Christian\\Documents\\GitHub\\Complexity_Metrics\\test\\translate2.java");
 		main.print();
 	}
 
-	
+
 }
