f52fe35bc66886404cc3da71cfb033c5afa08f76
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-09-19 18:23:03 +0200

    Create MicrotaskLoader.java

---
+++ src/MicrotaskLoader.java
@@ -0,0 +1,15 @@
+
+/**
+ * Load microtasks from a csv file and source code files
+ * The csv file contains the start and end line of the source code of each microtask
+ * The source code file contains the code of the Java method
+ * 
+ * @author Christian Adriano
+ *
+ */
+public class MicrotaskLoader {
+
+	
+	
+	
+}
