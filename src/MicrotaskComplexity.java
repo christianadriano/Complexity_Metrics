1c0f30073be9efa980a63431b75d1467e4bc49ae
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2019-09-19 18:22:56 +0200

    Create MicrotaskComplexity.java

---
+++ src/MicrotaskComplexity.java
@@ -0,0 +1,21 @@
+
+/** 
+ * Represents a microtask with regards to its complexity.
+ * It keeps infomation about the source code and the metrics for one microtask
+ * 
+ * @author Christian
+ *
+ */
+public class MicrotaskComplexity {
+
+	String microtaskID="";
+	String fileName="";
+	StringBuffer sourceCode=new StringBuffer();
+	Integer startLine=0;
+	Integer endLine=0;
+	
+	HalsteadMetrics metrics=null;
+	
+	
+	
+}
